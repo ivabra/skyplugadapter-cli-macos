ff0dd49fd5eb0be2748573b00de6b98431f36373
Ivan Brazhnikov <samsungpc239@gmail.com>
2017-11-18 20:08:24 +0300

    Initial Commit

---
+++ PowerAdapterConnector/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PowerAdapterConnector
+//
+//  Created by Ivan Brazhnikov on 18/11/2017.
+//  Copyright © 2017 Go About. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
